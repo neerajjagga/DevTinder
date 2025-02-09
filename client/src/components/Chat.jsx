b5acffe24ac090c7cf9f3fed11c29f63b734274f
Neeraj Jagga <neerajjagga26@gmail.com>
2025-02-10 01:58:22 +0530

    added chat functionality

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,13 +1,47 @@
 import { useConnectionStore } from "../store/connection.store";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createSocketConnection } from "./../lib/socket";
+import { useUserStore } from "./../store/user.store";
 
 const Chat = () => {
     const { selectedUserForChat } = useConnectionStore();
+    const { user } = useUserStore();
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        setMessages([]);
+        if (selectedUserForChat) {
+            const newSocket = createSocketConnection();
+            newSocket.emit("joinChat", { toUserId: user.id, fromUserId: selectedUserForChat.id });
+
+            newSocket.on("newMessageReceived", ({ message, fromUserId }) => {
+                if (fromUserId !== user.id) {
+                    setMessages((prevMessages) => [...prevMessages, { fromUserId, message }]);
+                }
+            });
+
+            setSocket(newSocket);
+
+            return () => {
+                newSocket.off("newMessageReceived");
+                newSocket.disconnect();
+            };
+        }
+    }, [selectedUserForChat]);
 
     const sendMessage = () => {
+        if (socket && message.trim() !== "") {
+            socket.emit("sendMessage", {
+                toUserId: selectedUserForChat.id,
+                fromUserId: user.id,
+                message
+            });
 
+            setMessages((prevMessages) => [...prevMessages, { fromUserId: user.id, message }]);
+            setMessage("");
+        }
     };
 
     if (!selectedUserForChat) {
@@ -19,7 +53,7 @@ const Chat = () => {
     }
 
     return (
-        <div className="h-screen flex flex-col">
+        <div className="h-full flex flex-col">
             <div className="p-3 bg-gray-800 text-white flex items-center gap-3">
                 <img
                     src={selectedUserForChat.profileImageUrl || 'https://res.cloudinary.com/dabywmj68/image/upload/v1738950098/0684456b-aa2b-4631-86f7-93ceaf33303c_gckgiv.jpg'}
@@ -29,8 +63,15 @@ const Chat = () => {
                 <h2 className="text-lg">{selectedUserForChat.name}</h2>
             </div>
 
-            <div className="flex-1 p-4 overflow-y-auto bg-gray-100">
-                
+            <div className="flex-1 p-4 overflow-y-auto bg-gray-100 flex flex-col gap-2">
+                {messages.map((chat, index) => (
+                    <div
+                        key={index}
+                        className={`px-4 py-2 rounded-lg max-w-xs ${chat.fromUserId === user.id ? "bg-blue-500 text-white self-end" : "bg-gray-300 self-start"}`}
+                    >
+                        {chat.message}
+                    </div>
+                ))}
             </div>
 
             <div className="p-3 bg-gray-200 flex items-center">
--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useConnectionStore } from "../store/connection.store";
 import Chat from '../components/Chat';
+import { PanelRight } from 'lucide-react';
 
 const ChatPage = () => {
     const { getMyConnections, myConnections, loading, setSelectedUserForChat } = useConnectionStore();
+    const [isSidebarActive, setIsSidebarActive] = useState(true);
 
     useEffect(() => {
         setSelectedUserForChat(null);
@@ -11,27 +13,36 @@ const ChatPage = () => {
     }, []);
 
     return (
-        <div className="w-full h-screen grid grid-cols-10">
-            <section className="col-span-3">
-                <div className="h-full bg-red-400 py-4">
+        <div className="w-full h-screen flex">
+            <section className={`fixed h-full bg-gray-800 transition-all ease-in ${isSidebarActive ? "w-56" : "max-w-16"}`}>
+                <div className="h-full bg-gray-900 py-4">
                     {loading ? (
                         <p>Loading connections...</p>
                     ) : (
-                        <div>
-                            {myConnections.map(connection => (
-                                <div key={connection.id} onClick={() => setSelectedUserForChat(connection)} className="flex gap-2 items-center bg-gray-400 p-2 cursor-pointer border-b-2 border-gray-600">
-                                    <div className="shrink-0">
-                                        <img className="h-12 rounded-full" src={connection.profileImageUrl ? connection.profileImageUrl : 'https://res.cloudinary.com/dabywmj68/image/upload/v1738950098/0684456b-aa2b-4631-86f7-93ceaf33303c_gckgiv.jpg'} alt="" />
+                        <>
+                            <div className={`w-full p-2 ${isSidebarActive ? 'text-end ' : 'text-center'}`}>
+                                <button className="hover:bg-gray-700 rounded-full transition ease-out p-2" onClick={() => setIsSidebarActive(prev => !prev)}>
+                                    <PanelRight size={28} />
+                                </button>
+                            </div>
+                            <div>
+                                {myConnections.map(connection => (
+                                    <div key={connection.id} onClick={() => setSelectedUserForChat(connection)} className="flex gap-2 items-center bg-gray-400 p-2 cursor-pointer border-b-2 border-gray-600">
+                                        <div className='shrink-0'>
+                                            <img className="h-12 rounded-full" src={connection.profileImageUrl ? connection.profileImageUrl : 'https://res.cloudinary.com/dabywmj68/image/upload/v1738950098/0684456b-aa2b-4631-86f7-93ceaf33303c_gckgiv.jpg'} alt="" />
+                                        </div>
+                                        {isSidebarActive && (
+                                            <span className="text-lg line-clamp-1">{connection.name}</span>
+                                        )}
                                     </div>
-                                    <span className="text-lg line-clamp-1">{connection.name}</span>
-                                </div>
-                            ))}
-                        </div>
+                                ))}
+                            </div>
+                        </>
                     )}
                 </div>
             </section>
 
-            <section className="col-span-7 h-full bg-green-400 w-full">
+            <section className={`transition-all z-20 ease-in ${isSidebarActive ? "ml-56" : "ml-16"} w-full`}>
                 <Chat />
             </section>
         </div>
